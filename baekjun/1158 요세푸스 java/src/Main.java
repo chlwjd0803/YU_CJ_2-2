21f68745c6d109fbd5cdbb5284024405914611e5
CJ <chlwjd0803@naver.com>
2024-10-08 22:56:58 +0900

    백준 1158 요세푸스 문제 해결

--- baekjun/1158 요세푸스 java/src/Main.java
+++ baekjun/1158 요세푸스 java/src/Main.java
@@ -1,24 +1,49 @@
 import java.util.Scanner;
 
-//%연산자 이용
-public class Main {
+class Queue{
+    private int [] arr; //사람 수
+    private int head;
+    private int tail;
+    public int size;
 
+    public Queue(int n){
+        size = n*2; //하나를 제거 후 뒤로 다 넘기므로 2배 용량이면 적합
+        arr = new int[size];
+        head = n;
+        tail = -1;
+        for(int i=0;i<n;i++) arr[i]=i+1; //i번째에 i+1번 사람 저장
+    }
+
+    public void enqueue(int x){
+        if(head == arr.length) head = 0;
+        arr[head++]=x;
+    }
+
+    public int dequeue(){
+        if(tail == arr.length-1) tail = -1; //해당 인덱스 자체에는 값이 없음?
+        return arr[++tail];
+    }
+}
+
+//%연산자 이용 - 큐 용량 늘리기
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         var n = sc.nextInt(); //총 인원수
         var k = sc.nextInt(); //제거 위치
-        int [] person = new int[n]; //사람 줄
-        int [] result = new int[n]; //결과 출력용
-        for(int i = 0; i < n; i++) person[i] = i+1; //1번부터 n번까지 지정
+        Queue q = new Queue(n);
+        int [] result = new int[n]; //인원수만큼
+        int cnt = 0; //인원수 만큼 차면 종료하기 위해 선언
 
-        var pos = 0;
-        var resn = 0;
-        int [] nextper = new int[n-resn];
-        while(resn < n){
-            if(pos == n) pos = 0; //칸을 넘어갈경우 0으로 돌아감
-            if((pos+1)%k == 0){ //인덱스와 1값 차이남
-                result[resn++] = person[pos];
+        for(int i=0; cnt!=n; i++){
+            if((i+1)%k==0) result[cnt++] = q.dequeue(); //디큐해서 result에 저장
+            else {
+                int temp = q.dequeue();
+                q.enqueue(temp); // 디큐해서 맨뒤에 엔큐
             }
         }
+        System.out.print("<");
+        for(int i=0; i<n-1; i++) System.out.print(result[i]+", ");
+        System.out.print(result[n-1]+">");
     }
 }
