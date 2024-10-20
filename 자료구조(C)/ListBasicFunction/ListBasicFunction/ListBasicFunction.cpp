0bc7f34d25448c7d143d17340bb8b2fbc72faefd
Jung Choi <chlwjd0803@naver.com>
2024-10-21 00:05:25 +0900

    자료구조 리스트 기본 구현 및 클래스 분리 추가

---
+++ 자료구조(C)/ListBasicFunction/ListBasicFunction/ListBasicFunction.cpp
@@ -0,0 +1,16 @@
+﻿#include "List.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	List l;
+
+	l.add(10);
+	l.add(20);
+	l.add(30);
+	l.insert(50, 20); //50을 20뒤에 추가
+	l.dlt(30);
+
+	l.printList();
+}
