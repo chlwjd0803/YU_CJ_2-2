a3087269cd269357da617fe9fec282b2c1473d0c
jung choi <chlwjd0803@naver.com>
2024-11-05 14:05:19 +0900

    자료구조실습 과제6.4 merge 구현완료

--- 자료구조실습(C++)/Lab_6/HW6.4/HW6.4/List.h
+++ 자료구조실습(C++)/Lab_6/HW6.4/HW6.4/List.h
@@ -12,8 +12,7 @@ public:
 
 	List* first; // 처음 리스트는 비어 있음
 
-	List(int n = 0) {
-		data = n;
+	List() {
 		link = nullptr;
 		first = nullptr;
 	}
@@ -28,41 +27,40 @@ public:
 	}
 
 	void insert(int item) {
-		List* temp = new List(item); //원소는 item으로 미리 줌, link는 NULL
+		List* temp = new List();
+		temp->data = item;
 		List* ptr;
 
 		if (first == nullptr) {
 			first = temp;
 		}
-		/*
+
 		else {
-			for (ptr = first; ptr->link != nullptr; ptr = ptr->link); //추적하여 뒤에 붙임
-			ptr->link = temp;
-			temp->link = nullptr;
-		}
-		*/
-		else {
+			if (temp->data <= first->data) {
+				temp->link = first;
+				first = temp;
+				return;
+			}
+
 			ptr = first;
 			for (; ptr->link != nullptr; ptr = ptr->link) {
 				if ((ptr->data) <= (temp->data) && ((ptr->link->data) >= (temp->data))) {
 					//ptr과 ptr->link 사이의(같은조건도 포함) 값이면 삽입
 					temp->link = ptr->link;
 					ptr->link = temp;
-					break; //삽입이 끝났으므로 그냥 나가기
+					return; //삽입이 끝났으므로 그냥 나가기
 				}
 			}
-			if (ptr->link == nullptr) { //모두 비교하였는데 아닌경우 그냥 맨 뒤에다가 붙임
-				ptr->link = temp;
-				temp->link = nullptr;
-			}
+
+			ptr->link = temp;
+			temp->link = nullptr;
 		}
 	}
-
-	List* merge(List* oth) {
-		List* orig = first;
-		List* temp = oth;
-		for (; temp == nullptr; temp = temp->link) { insert(temp->data); }
-		return this;
+	
+	void merge(List* oth) {
+		List* orig = this->first; //A객체의 함수에서 실행하였으므로 그대로 사용해도됨(또는 this로 명시)
+		List* temp = oth->first; //매개변수로 가져왔으므로 B객체의 것이라고 명시해야함(중요함)
+		for (; temp != nullptr; temp = temp->link) { insert(temp->data); }
 	}
 
 	void printList() {
