f3fa6ca56f67c24faf079d30a3cb361e9504b129
jung choi <chlwjd0803@naver.com>
2024-12-02 17:34:30 +0900

    자구실 10번완성

--- 자료구조실습(C++)/Lab_10/HW10/HW10/HW10.cpp
+++ 자료구조실습(C++)/Lab_10/HW10/HW10/HW10.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-short int visited[MAX_VERTICES];
-
+short int visited[MAX_VERTICES]; //short int나 bool이나 같은 기능을 하기 위함
+int parent[MAX_VERTICES]; //부모노드를 저장하는 곳, 신장트리 출력을 위해 선언
 
 class Node {
 public:
@@ -45,14 +45,51 @@ int printList(int index, Node* temp) {
 void dfs(int v) {
 	Node* w;
 	visited[v] = TRUE;
-	printf("%5d", v);
+	printf("%4d", v);
 	for (w = graph[v]; w; w = w->link) {
-		if (!visited[w->data]) dfs(w->data);
+		if (!visited[w->data]) {
+			parent[w->data] = v;
+			dfs(w->data);
+		}
 	}
+}
+
+void bfs(int v) {
+	Node* w;
+	int queue[MAX_VERTICES];
+	int front = -1;
+	int rear = -1;
+
+	printf("%4d", v);
+	visited[v] = TRUE;
+	queue[++rear] = v;
+	while (front != rear) {
+		v = queue[++front];
+		for (w = graph[v]; w; w = w->link) {
+			if (!visited[w->data]) {
+				printf("%4d", w->data);
+				queue[++rear] = w->data;
+				visited[w->data] = TRUE;
+				parent[w->data] = v;
+			}
+		}
+	}
+}
+
+void printSpanningTree(int nodeNum) {
+	for (int i = 0; i < nodeNum; ++i){ 
+		if (parent[i] != -1){
+			printf("(%d, %d) ", parent[i], i); 
+		} 
+	} 
+	printf("\n");
 }
 
 
 int main() {
+
+	//사전작업
+	
 	
 	printf("HW10.1 인접 리스트 형태의 무방향성 그래프 생성\n");
 	printf("노드 개수 : ");
@@ -60,9 +97,8 @@ int main() {
 	scanf("%d", &nodeNum);
 
 	graph = new Node*[nodeNum];
-	for (int i = 0; i < nodeNum; i++) {
-		graph[i] = NULL; //일단 NULL로 모두 초기화
-	}
+	for (int i = 0; i < nodeNum; i++) graph[i] = NULL; //일단 NULL로 모두 초기화
+	for (int i = 0; i < MAX_VERTICES; i++) parent[i] = -1; //처음에 0이 읽히지 않게 제대로 초기화, HW10.3 전용
 
 	int vi, vj;
 	while (true) {
@@ -86,21 +122,46 @@ int main() {
 		}
 	}
 
-	printf("degree가 가장 큰 노드와 그 값 : ");
+	printf("\ndegree가 가장 큰 노드와 그 값 : ");
 	printf("노드 %d, degree %d\n", max_idx, max);
 	printf("노드 %d와 연결된 노드들 : ", max_idx);
-	printList(max_idx, graph[max_idx]); //수정하셈
+	printList(max_idx, graph[max_idx]);
 
-	printf("HW10.2 탐색알고리즘\n");
-	//v는 시작노드
-	for (int i = 0; i < nodeNum; i++) {
-		printf("시작노드 %d : ", i);
+	printf("\nHW10.2/10.3 탐색알고리즘과 신장트리\n");
+	
+	printf("깊이 우선 탐색 DFS\n");
+	for (int i = 0; i < nodeNum; i+=2) {
+		printf("\n시작노드 %d : ", i);
 		dfs(i);
-		printf("\n");
+		printf("\nDFS 신장 트리 : ");
+		printSpanningTree(nodeNum);
+		for (int j = 0; j < MAX_VERTICES; j++) {
+			visited[j] = FALSE; //방문기록 삭제
+			parent[j] = -1; //부모노드 초기화
+		}
+	}
+	printf("\n너비 우선 탐색 BFS\n");
+	for (int i = 1; i < nodeNum; i += 2) {
+		printf("\n시작노드 %d : ", i);
+		bfs(i);
+		printf("\nBFS 신장 트리 : ");
+		printSpanningTree(nodeNum);
+		for (int j = 0; j < MAX_VERTICES; j++) {
+			visited[j] = FALSE; //방문기록 삭제
+			parent[j] = -1; //부모노드 초기화
+		}
 	}
 
-
-
+	//메모리 정리과정, 필수는 아님
+	for (int i = 0; i < nodeNum; i++) {
+		Node* temp = graph[i];
+		while (temp) {
+			Node* del = temp; 
+			temp = temp->link; 
+			delete del; 
+		} 
+	} 
+	delete[] graph;
 
 	return 0;
 }
